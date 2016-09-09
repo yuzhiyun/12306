283ff55b4650ca79bb81ef48f54c5b376e3e1647
yuzhiyun <2723364262@qq.com>
2016-09-09 20:36:47 +0800

    优化部分界面

--- app/src/main/java/com/suk/yuzhiyun/my12306/reserve/MainActivity.java
+++ app/src/main/java/com/suk/yuzhiyun/my12306/reserve/MainActivity.java
@@ -1,7 +1,11 @@
 package com.suk.yuzhiyun.my12306.reserve;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
@@ -24,6 +28,8 @@ public class MainActivity extends BaseActivity {
     private static final int REQUEST_CODE = 1;
     @Bind(R.id.tvStartTime)
     TextView tvStartTime;
+    @Bind(R.id.tvStartHour)
+    TextView tvStartHour;
 
     @Override
     protected void setLayoutView() {
@@ -35,12 +41,34 @@ public class MainActivity extends BaseActivity {
         year = DateUtil.getYear();
         month = DateUtil.getMonth();
         day = DateUtil.getCurrentMonthDay();
-        tvStartTime.setText(year + "-" + month + "-" + day);
+        tvStartTime.setText(year + " - " + month + " - " + day);
     }
 
     @OnClick(R.id.tvStartTime)
     public void setTvStartTime() {
         startActivityForResult(new Intent(context, CalendarActivity.class), REQUEST_CODE);
+    }
+
+    String[] hours = new String[]{
+            "00:00 - 24:00",
+            "00:00 - 06:00",
+            "06:00 - 12:00",
+            "12:00 - 18:00",
+            "18:00 - 24:00",
+    };
+
+    @OnClick(R.id.tvStartHour)
+    public void setTvStartHour() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        builder.
+        builder.setItems(hours, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tvStartHour.setText(hours[which]);
+            }
+        });
+        builder.create().show();
+
     }
 
     @Override
@@ -51,7 +79,7 @@ public class MainActivity extends BaseActivity {
                 year = data.getIntExtra("year", 0);
                 month = data.getIntExtra("month", 0);
                 day = data.getIntExtra("day", 0);
-                tvStartTime.setText(year + "-" + month + "-" + day);
+                tvStartTime.setText(year + " - " + month + " - " + day);
             }
 
 
