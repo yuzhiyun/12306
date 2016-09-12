1cee56f7d40f5ac173cbf552295082c017870902
yuzhiyun <2723364262@qq.com>
2016-09-12 14:43:54 +0800

    查询站点

--- app/src/main/java/com/suk/yuzhiyun/my12306/inquire/MainActivity.java
+++ app/src/main/java/com/suk/yuzhiyun/my12306/inquire/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.suk.yuzhiyun.my12306.R;
 import com.suk.yuzhiyun.my12306.base.BaseActivity;
@@ -18,7 +21,7 @@ import com.suk.yuzhiyun.my12306.inquire.viewpager.ViewPagerFragment;
 import butterknife.Bind;
 import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener{
+public class MainActivity extends BaseActivity implements ViewPager.OnPageChangeListener {
 
 
     @Bind(R.id.vPager)
@@ -29,16 +32,33 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
     int day;
 
     private static final int REQUEST_CODE = 1;
+    private static final int REQUEST_START_CITY_CODE = 2;
+    private static final int REQUEST_END_CITY_CODE = 3;
+    @Bind(R.id.tvStartCity)
+    TextView tvStartCity;
+    @Bind(R.id.tvEndCity)
+    TextView tvEndCity;
+
+    String startCity = null;
+    String endCity = null;
+
+
     @Bind(R.id.tvStartTime)
     TextView tvStartTime;
 
-    @Bind(R.id.tvStartHour)
-    TextView tvStartHour;
+    @Bind(R.id.tvSeat)
+    TextView tvSeat;
+
+    //小圆点图片
+    private ImageView[] imgVIndicator;
+    //viewPager导航
+    @Bind(R.id.linearIndicator)
+    LinearLayout linearIndicator;
     /**
      * 处理viewpager自动轮播
      */
     int currentItem = 0;
-    int AUTO_MESSAGE=1;
+    int AUTO_MESSAGE = 1;
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -64,13 +84,56 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
         initViewPager();
 
         //viewpager开始轮播
-        handler.sendEmptyMessageDelayed(AUTO_MESSAGE,1000);
+        handler.sendEmptyMessageDelayed(AUTO_MESSAGE, 1000);
 
+        setIndicator();
 
+    }
+
+    /**
+     * viewpager导航图片设置
+     */
+    private void setIndicator() {
+        vPager.setOnPageChangeListener(this);
+
+        int size = ViewPagerFragment.picture.length;
+        imgVIndicator = new ImageView[size];
+        for (int i = 0; i < size; i++) {
+
+            final ImageView imageView = new ImageView(this);
+
+            imgVIndicator[i] = imageView;
+
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            //默认选择第一张图片
+            if (0 == i)
+                imgVIndicator[i].setImageResource(R.drawable.selected_circle);
+            else
+                imgVIndicator[i].setImageResource(R.drawable.unselected_circle);
+            LayoutParams lp = new LayoutParams(15, 15);
+            lp.setMargins(15, 0, 0, 0);
+            linearIndicator.addView(imgVIndicator[i], lp);
+        }
     }
 
     private void initViewPager() {
         vPager.setAdapter(new Adapter(getSupportFragmentManager()));
+    }
+
+    /**
+     * 选择起始站
+     */
+    @OnClick(R.id.tvStartCity)
+    public void setTvStartCity() {
+        startActivityForResult(new Intent(context, ChooseCityActivity.class), REQUEST_START_CITY_CODE);
+    }
+
+    /**
+     * 选择终点站
+     */
+    @OnClick(R.id.tvEndCity)
+    public void setTvEndCity() {
+        startActivityForResult(new Intent(context, ChooseCityActivity.class), REQUEST_END_CITY_CODE);
     }
 
     @OnClick(R.id.tvStartTime)
@@ -78,22 +141,23 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
         startActivityForResult(new Intent(context, CalendarActivity.class), REQUEST_CODE);
     }
 
-    String[] hours = new String[]{
-            "00:00 - 24:00",
-            "00:00 - 06:00",
-            "06:00 - 12:00",
-            "12:00 - 18:00",
-            "18:00 - 24:00",
+    String[] seats = new String[]{
+            "硬    座",
+            "软    座",
+            "硬    卧",
+            "软    卧",
+            "特   等  座",
+            "商   务  座"
     };
 
-    @OnClick(R.id.tvStartHour)
-    public void setTvStartHour() {
+    @OnClick(R.id.tvSeat)
+    public void setSeat() {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 //        builder.
-        builder.setItems(hours, new DialogInterface.OnClickListener() {
+        builder.setItems(seats, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                tvStartHour.setText(hours[which]);
+                tvSeat.setText(seats[which]);
             }
         });
         builder.create().show();
@@ -103,6 +167,7 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //选择日期
         if (requestCode == REQUEST_CODE)
             if (resultCode == CalendarActivity.RESULT_OK) {
                 year = data.getIntExtra("year", 0);
@@ -111,14 +176,23 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
                 tvStartTime.setText(year + " - " + month + " - " + day);
             }
 
-
+        //选择车站
+        if (requestCode == REQUEST_START_CITY_CODE)
+            if (resultCode == ChooseCityActivity.RESULT_CHOOSE_CITY) {
+                startCity = data.getStringExtra("city");
+                tvStartCity.setText(startCity);
+            }
+        //选择终点车站
+        if (requestCode == REQUEST_END_CITY_CODE)
+            if (resultCode == ChooseCityActivity.RESULT_CHOOSE_CITY) {
+                endCity = data.getStringExtra("city");
+                tvEndCity.setText(endCity);
+            }
     }
-
     /**
      * 以下三个方法是继承接口ViewPager.OnPageChangeListener后
      * 需要实现的方法
-     *
-     * */
+     */
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -126,7 +200,14 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
 
     @Override
     public void onPageSelected(int position) {
+        int size = ViewPagerFragment.picture.length;
+        for (int i = 0; i < size; i++) {
 
+            if (position == i)
+                imgVIndicator[i].setImageResource(R.drawable.selected_circle);
+            else
+                imgVIndicator[i].setImageResource(R.drawable.unselected_circle);
+        }
     }
 
     @Override
